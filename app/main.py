e01ded87b1b5775676e0deb4401d6e6eff45f4ed
venkywaldek <venkatneoiit@gmail.com>
2026-03-30 12:06:37 +0300

    CC code challenge 3

--- app/main.py
+++ app/main.py
@@ -12,14 +12,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return { "msg": "Hello Venkat ,local docker!", "v": "0.1" }
+# @app.get("/")
+# def read_root():
+#     return { "msg": "Hello Venkat ,local docker!", "v": "0.1" }
 
 
 @app.get("/hello")
 def hello():
-    return {"msh": "Hello venkat"}
+    return {"msg": "Hello venkat"}
+
+
+
+rooms = [ {"number" : 1, "persons" : 1  , "name" : "singleroom"}, {"number" : 2, "persons" : 2  , "name" : "doubleroom"},  {"number" : 3, "persons" : 3  , "name" : "Suiteroom"}]
+ 
+@app.get("/items/rooms")
+def read_root():
+    return rooms
+
+@app.get("/hotel")
+
+def hotel():
+    return {"msg": "Hello Hotel"}
+
+
 
 @app.get("/api/ip")
 def ip(request: Request):
