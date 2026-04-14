3355e1af6d0d57327e549aaea29a85f885df8d89
venkywaldek <venkatneoiit@gmail.com>
2026-04-14 23:55:00 +0300

    Hotel fronend  CC

---
+++ app/db.py
@@ -0,0 +1,53 @@
+import os
+import psycopg
+from psycopg import rows
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_conn():
+  return psycopg.connect(DATABASE_URL, autocommit=True, row_factory=rows.dict_row)
+
+def create_schema():
+   with get_conn() as conn, conn.cursor() as cur:
+            # Create the schema
+            cur.execute("""
+                -- sample parent table
+                CREATE TABLE IF NOT EXISTS hotel_rooms (
+                    id SERIAL PRIMARY KEY, 
+                    room_number INT NOT NULL, 
+                    room_type VARCHAR NOT NULL,
+                    price INT NOT NULL
+                );
+                 
+                ---------  
+                 -- guests
+                ---------
+                CREATE TABLE IF NOT EXISTS hotel_guests (
+                    id SERIAL PRIMARY KEY, -- primary key,
+                    first_name  VARCHAR NOT NULL,
+                    last_name  VARCHAR NOT NULL,
+                    created_at TIMESTAMP DEFAULT now(),
+                    address VARCHAR
+                );
+               -- add columns
+                --  ALTER TABLE hotel_guests  created_at TIMESTAMP DEFAULT now();
+                
+                 ---------  
+                 -- bookings
+                ---------
+                -- bookings
+                CREATE TABLE IF NOT EXISTS hotel_bookings (
+                  id SERIAL PRIMARY KEY,
+                  guest_id INT NOT NULL REFERENCES hotel_guests(id), -- foreign key
+                  room_id INT NOT NULL REFERENCES hotel_rooms(id),
+                  datefrom DATE DEFAULT now() NOT NULL,
+                  dateto DATE NOT NULL,
+                  addinfo VARCHAR
+                  );
+                  
+                  -- add columns
+                  -- man kan sätta default också senare
+                  -- ALTER TABLE hotel_bookings  datefrom  DEFAULT now();
+                  -- lägg till främmande nyckel senare:
+                  -- ADD CONSTRAINT hotel_guest_id_key FOREIGN KEY (guest_id) REFERENCES
+            """)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+from app.db import get_conn, create_schema
 
 app = FastAPI()
+
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
@@ -12,31 +16,94 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# @app.get("/")
-# def read_root():
-#     return { "msg": "Hello Venkat ,local docker!", "v": "0.1" }
+create_schema()
 
-#tillfälligt data
+class Booking(BaseModel):
+    guest_id: int
+    room_id: int
+    datefrom: date
+    dateto: date
+    addinfo: Optional[str] = None
 
-temp_rooms = [ 
-         {"room_number" : 101, "price" : 1  , "room_type" : "double room"}, 
-         {"room_number" : 202, "price" : 2  , "room_type" : "single room"}, 
-         {"room_number" : 303, "price" : 3  , "room_type" : "Suite"}
-    ]
- 
 @app.get("/")
-def read_root():
-    return("Välkommen till hötell  API")
+def root():
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT 'databasen funkar' as msg
+        """)
+        result = cur.fetchone()
+    return {"msg": "Välkommen till hostells bokning API", "db": result["msg"]}
 
 @app.get("/rooms")
 def rooms():
-    return  temp_rooms
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT *
+            FROM hotel_rooms
+            ORDER BY room_number
+        """)
+        rooms = cur.fetchall()
+    return rooms
 
-@app.put("/rooms")
-def rooms():
-    return  temp_rooms
+@app.get("/rooms/{id}")
+def get_room(id: int):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT *
+            FROM hotel_rooms
+            WHERE id = %s
+        """, (id,))
+        room = cur.fetchall()
+    return room
+
+@app.get("/bookings")
+def get_bookings():
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                hb.id,
+                hb.guest_id,
+                hb.room_id,
+                hb.datefrom,
+                hb.dateto,
+                hb.addinfo,
+                hr.room_number
+            FROM hotel_bookings hb
+            JOIN hotel_rooms hr ON hb.room_id = hr.id
+            ORDER BY hb.datefrom, hb.dateto
+        """)
+        bookings = cur.fetchall()
+    return bookings
 
 @app.post("/bookings")
-def create_booking():
-    return{"msg" : "booking skapad"}
+def create_booking(booking: Booking):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO hotel_bookings (
+                guest_id,
+                room_id,
+                datefrom,
+                dateto,
+                addinfo
+            ) VALUES (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            )
+            RETURNING *
+        """, (
+            booking.guest_id,
+            booking.room_id,
+            booking.datefrom,
+            booking.dateto,
+            booking.addinfo
+        ))
+        new_booking = cur.fetchone()
 
+    return {"msg": "booking created", "booking": new_booking}
+
+@app.on_event("startup")
+def startup():
+    create_schema()
